9c928bfa11bcaf9ebf5c60ea479f1dfd38cc2cf0
Nancy Jackson <nbjackson23@gmail.com>
2020-08-05 23:01:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+var katzDeli = []; // At the beginning of the day,
+//the deli is empty and is represented by an empty array
+//<<<<<<<<<--------------- in the instructions <<<<<<
+
+function takeANumber(katzDeli, name) {
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
